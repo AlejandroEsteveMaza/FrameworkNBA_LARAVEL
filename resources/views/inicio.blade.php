563d4159a7fab94c82c563aaeef2fdce407c07ca
Alejandro Esteve Maza <54013692d@AD.FPMISLATA.COM>
2019-12-04 09:21:17 +0100

    primeras vistas

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+
+    <?php
+    include "topBar.php";
+    //@include('topBar.php');
+    ?>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/inicio.css')}}">
+
+</head>
+
+<body>
+    <img src="{{asset('images/celticsEscudo.jpg')}}" alt="Celtics Escudo" class="logo--center">
+</body>
+
+</html>
